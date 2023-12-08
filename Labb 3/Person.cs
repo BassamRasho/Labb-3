c36d89429020c452c1a7243bdf12f4bdbe5fafaf
Bassa <Bassa@BASSAM>
2023-12-08 10:58:26 +0100

    Add project files.

---
+++ Labb 3/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3
+{
+    public class Person
+    {
+        public string Name { get; private set; } 
+
+        public Hair HairStyle { get; private set; }
+        
+        public Gender TheGender { get; private set; }
+
+        public DateTime Birthday { get; private set; }
+
+        public string EyesColor { get; private set; }
+
+
+        public Person (string name, Hair hair, Gender gender, DateTime birthday,string eyescolor)
+        {
+            Name = name;
+            HairStyle = hair;
+            TheGender = gender; 
+            Birthday = birthday;
+            EyesColor = eyescolor;
+           
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}" +
+                $"\nHair: {HairStyle.HairColor} {HairStyle.HairLength}" +
+                $"\nSex: {TheGender}" +
+                $"\nBirthday: {Birthday:yyyy/MM/dd}" +
+                $"\nEyesColor: {EyesColor}";
+             
+
+
+        }
+      
+    }
+}
---
+++ Labb 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Labb_3;
+
+namespace Labb_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            String name = "Bassam Rasho";
+            Gender gender = Gender.Man;
+            Hair hair = new Hair { HairColor = "Brown", HairLength = "35 cm long" };
+            DateTime birthday = new DateTime(1998, 5, 6);
+            string eyesColor = "Brown";
+
+            Person person = new Person(name, hair, gender, birthday, eyesColor);
+
+            Console.WriteLine(person.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
